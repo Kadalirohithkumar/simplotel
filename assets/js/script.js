eebc302251474b7c3e0197ececdc1a15bbf7ea5b
Kadalirohithkumar <96775264+Kadalirohithkumar@users.noreply.github.com>
2024-02-20 18:34:15 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+/*============================================================================
+  ============================================================================*/
+
+    // Navigation
+        // Responsive Toggle Navigation =============================================
+        let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
+
+
+        function changeBg() {
+          var indi = document.getElementById("indi");
+          var scrollValue = window.scrollY;
+          if (scrollValue < 400) {
+            indi.style.display = "none";
+          } else if (scrollValue > 400) {
+            indi.style.display = "block";
+          }
+        }
+        changeBg();
+
+        window.addEventListener("scroll", changeBg);
